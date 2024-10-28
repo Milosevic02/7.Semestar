75ffc353af91748514f0dafe4589d4fc5b69814c
Dragan <milosevicdragan002@gmail.com>
2024-10-28 10:44:30 +0100

    Update CertManager

--- Bezbednost/Vezba_2/Manager/CertManager.cs
+++ Bezbednost/Vezba_2/Manager/CertManager.cs
@@ -18,9 +18,20 @@ namespace Manager
 		/// <returns> The requested certificate. If no valid certificate is found, returns null. </returns>
 		public static X509Certificate2 GetCertificateFromStorage(StoreName storeName, StoreLocation storeLocation, string subjectName)
 		{
-			X509Certificate2 certificate = null;
+			X509Store store = new X509Store(storeName,storeLocation);
+			store.Open(OpenFlags.ReadOnly);
 
-			return certificate;
+			X509Certificate2Collection certCollection = store.Certificates.Find(X509FindType.FindBySubjectName, subjectName, true);
+
+			foreach (X509Certificate2 cert in certCollection)
+			{
+				if (cert.SubjectName.Name.Equals(string.Format("CN={0}", subjectName)))
+				{
+					return cert;
+				}
+			}
+
+			return null;
 		}
 	}
 }
