1ab5d7ec188b4ae2d97f423fc14e3da8d8c03f9b
Dragan <milosevicdragan002@gmail.com>
2024-10-28 10:25:41 +0100

    Vezbe2 template

---
+++ Bezbednost/Vezba_2/Manager/CertManager.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Security.Cryptography.X509Certificates;
+
+namespace Manager
+{
+	public class CertManager
+	{
+		/// <summary>
+		/// Get a certificate with the specified subject name from the predefined certificate storage
+		/// Only valid certificates should be considered
+		/// </summary>
+		/// <param name="storeName"></param>
+		/// <param name="storeLocation"></param>
+		/// <param name="subjectName"></param>
+		/// <returns> The requested certificate. If no valid certificate is found, returns null. </returns>
+		public static X509Certificate2 GetCertificateFromStorage(StoreName storeName, StoreLocation storeLocation, string subjectName)
+		{
+			X509Certificate2 certificate = null;
+
+			return certificate;
+		}
+	}
+}
